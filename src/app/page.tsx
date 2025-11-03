3cac7c254939f715b23cb7fccbef027bf0e47632
Dawitay <dawitayelewoldesenbet@gmail.com>
2025-11-03 09:24:46 +0300

    install next js and tailwind css

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import { metadata } from "./layout"
+
+
+
+export default function Home() {
+  return (
+    <>
+    <div>
+      
+      <h1 className="text-red-400 bg-red-900 text-3xl font-bold underline m-15">
+        Hello world!
+      </h1>
+    </div>
+    </>
+  )
+}
