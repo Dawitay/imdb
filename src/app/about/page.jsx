3c70c8c517e58745d10fc698bd67e8967218c71e
Dawitay <dawitayelewoldesenbet@gmail.com>
2025-11-04 22:06:09 +0300

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>
+        እንኳን ወደ IMDb Dawit በኔ ዳዊት አየለ ለመማሪያ ወደ ተሠራው ድረ ገፅ በደኅና መጡ።
+        ይህ NextJs እያጠናው የሠራውት ፕሮጀክት ሲሆን ስለ NextJs 16 የተለያዩ መሠረታዊ ነገሮችን አግኝቼበታለሁ።
+      </p>
+      <p>
+        ይህ ገጽ የተለያዩ የነጭ ሰዎችን ፊልም በቀላሉ ፈልጎ ለማግኘት የሚረዳን ሲሆን አዳዲስ እንዲሁም በሰዎች ዘንድ እጅግ የተወደዱትን ለይቶ ስለሚያቀርብ በቀላሉ ለማግኘት ይረዳዎታል።
+      </p>
+    </div>
+  )
+}
